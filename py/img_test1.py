dfea913efa1e3b2fdfd41c0d7dc8f611b9159466
theUnspecified <222mzb@gmail.com>
2017-04-28 17:43:25 +0600

    28/4

---
+++ py/img_test1.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+
+print(os.getcwd())
+
+os.chdir("..\\scraping_nasa_server\\tempa_image")
+
+files = os.listdir()
+j = 1
+for image in files:
+    i = cv2.imread(image)
+    temp = i[204:271, 188:305]
+    cv2.imwrite('%d.jpg' %1, temp)
+    j+=1
+
